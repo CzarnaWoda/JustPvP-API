d7ccf04c95bd6bfdaf3d4fc0724a17b2d2669570
CzarnaWoda <38265586+CzarnaWoda@users.noreply.github.com>
2018-06-07 18:04:07 +0200

    Update
    Config Update

--- JustPvP-API/src/pl/blackwaterapi/configs/ConfigCreator.java
+++ JustPvP-API/src/pl/blackwaterapi/configs/ConfigCreator.java
@@ -3,10 +3,13 @@ package pl.blackwaterapi.configs;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
@@ -29,6 +32,65 @@ public abstract class ConfigCreator {
 	}
 	public Plugin getPlugin(){
 		return plugin;
+	}
+	public Object addField(String path, Object o){
+		getConfig().addDefault(path, o);
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+		return o;
+	}
+	public void createSection(String section){
+		getConfig().createSection(section);
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+	}
+	public void addToSection(String section,String path, Object o){
+		ConfigurationSection sectionc = getConfigurationSection(section);
+		if(sectionc== null){
+			createSection(section);
+		}
+		getConfig().getConfigurationSection(section).addDefault(path, o);
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+	}
+	public void addToSection(ConfigurationSection section,String path, Object o){
+		section.addDefault(path, o);
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+	}
+	public void setField(String path,Object o){
+		Object object = getConfig().get(path);
+		if(object == null){
+			addField(path, o);
+			getConfig().options().copyDefaults(true);
+		}
+		getConfig().set(path, o);
+		saveConfig();
+	}
+	public void addToListField(String path,String add){
+		List<String> list  = getConfig().getStringList(path);
+		if(list == null){
+			addField(path, Arrays.asList("default"));
+		}
+		list.add(add);
+		setField(path, list);
+		saveConfig();
+	}
+	public void addToListField(String path, int i){
+		List<Integer> list = getConfig().getIntegerList(path);
+		if(list == null){
+			addField(path, Arrays.asList(1));
+		}
+		list.add(i);
+		setField(path, list);
+		saveConfig();
+	}
+	public ConfigurationSection getConfigurationSection(String section){
+		ConfigurationSection sectionc = getConfig().getConfigurationSection(section);
+		if(sectionc == null){
+			createSection(section);
+		}
+		return getConfig().getConfigurationSection(section);
 	}
 	@SuppressWarnings("deprecation")
 	public void reloadConfig() {
